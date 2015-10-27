1bc5963c4ea6cdf047b5cc98d96bbcb4cc245498
Satish Kumar Kamavaram <satishkamavaram@gmail.com>
2015-10-27 22:15:17 +0530

    modified algorithm to take care all scenarios

--- src/com/satish/algorithms/Algorithm4.java
+++ src/com/satish/algorithms/Algorithm4.java
@@ -8,24 +8,59 @@ package com.satish.algorithms;
  */
 public class Algorithm4 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		runAlgorithm();
-	}
-	
-	private static void runAlgorithm()
-	{
-		int total = 1060;
-		int [] coins = new int []{50};
-		int i =0;
-		while(total>0 && i<coins.length)
-		{
-			int x = total/coins[i];
-			System.out.println("total number of "+coins[i]+" is "+x);
-			total = total%coins[i];
-			i++;
-		}
-				
-	}
+	int maxCoins = -1;
+    int totalAmt = 11;
+    int [] coinValues = {5,1,3,4,2,6,8,7,2};
+   
+    public static void main(String[] args) {
+           // TODO Auto-generated method stub
+    	   Algorithm4 coins = new Algorithm4();
+           coins.findArrayCombination(coins.coinValues,0);
+           System.out.println(coins.maxCoins);
+    }
+
+    private  void calculate(int [] coins)
+    {
+           int size = coins.length;
+           int total = totalAmt;
+           int numberOfCoins = 0;
+           for(int i =0;i<size;i++)
+           {
+                        if(total>0)
+                        {
+                               numberOfCoins +=total/coins[i];
+                               total = total%coins[i];
+                        }
+                        else
+                               break;
+           }
+          
+           if(maxCoins<0 || numberOfCoins < maxCoins)
+                  maxCoins = numberOfCoins;
+    }
+   
+    private  void findArrayCombination(int [] coins, int start)
+    {
+           if(start==coins.length)
+           {
+                  calculate(coins);
+                  return;
+           }
+          
+           for(int i =start;i<coins.length;i++)
+           {
+                  int temp = coins[start];
+                  coins[start] = coins[i];
+                  coins[i] = temp;
+                 
+                  findArrayCombination(coins,start+1);
+                 
+                  temp = coins[start];
+                  coins[start] = coins[i];
+                  coins[i] = temp;
+                 
+           }
+                 
+    }
 
 }
